78d4020232643dce131c9273f44f63d8cf3a0a7d
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-08-17 23:13:13 +0100

    basic iteration structure

--- src/lexer.c
+++ src/lexer.c
@@ -1,15 +1,61 @@
 // LIBRARY IMPORTS
-
+#include <stdio.h>
+#include <stdlib.h>
 
 // LOCAL IMPORTS
+#include <lexer.h>
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+char* readInput(char* path){
+
+
+    FILE* fp = fopen(path, "rb");
+
+    char* input = malloc(1);
+
+    int c;
+    int i = 0;
+
+    while((c = fgetc(fp)) != EOF){
+        input[i] = c;
+        i++;
+        input = realloc(input, i + 1);
+    }
+
+    return input;
+
+}
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 
+iterateScanner(scanner* s){
+
+    s->pos++;
+    s->currChar = s->input[pos];
+
+}
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+void skipWhiteSpace(scanner* s){
 
+    while (s->currChar == ' ' || s->currChar == '\n' || s->currChar == '\r' || s->currChar == '\t'){
+
+        if (s->currChar == '\n'){ s->line++;}
+        iterateScanner(s);
+
+    }
+
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+void lexer(){
+
+    scanner* s = calloc(1, sizeof(*s));
+
+}
 
 /*--------------------------------------------------------------------------------------------------------------------*/
