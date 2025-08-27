167b166605fc1d8c023a0e26b24a45117d5b24ee
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-08-27 22:47:28 +0100

    registers table

--- src/generator.c
+++ src/generator.c
@@ -10,6 +10,44 @@
 // resisters arr (useful as a queue (or stack)? -> pop off to use, put back when done)
 // ops arr
 
+static char* registers[31] = {
+    "X0",
+    "X1",
+    "X2",
+    "X3",
+    "X4",
+    "X5",
+    "X6",
+    "X7",
+    "X8",
+    "X9",
+    "X10",
+    "X11",
+    "X12",
+    "X13",
+    "X14",
+    "X15",
+    "X16",
+    "X17",
+    "X18",
+    "X19",
+    "X20",
+    "X21",
+    "X22",
+    "X23",
+    "X24",
+    "X25",
+    "X26",
+    "X27",
+    "X28",
+    "X29",
+    "X30",
+};
+
+static char* opcodes[10] = {
+    
+}
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 void exp(int operator, char* arg, char* arg); // construct asm expr
@@ -18,8 +56,23 @@ void exp(int operator, char* arg, char* arg); // construct asm expr
 
 void postOrderSearch(astNode* node){ // will give us left arg, right arg then op going bottom to top of tree
 
-    if (node->right == NULL){}
+    if (node->right == NULL){
+        // load value into reg on top of stack
+        // return reg
+    }
+
+
 
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+
+/* get result
+
+if number then result is reg number is loaded into
+
+if operator then result is reg result was written too
+
+
+*/
